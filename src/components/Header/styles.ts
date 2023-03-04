8c5ebbb9b680d964d77e66f677c3d37b58b983ef
Marcos Botene <marcosjbotene@gmail.com>
2023-03-04 17:30:27 -0300

    refactor: header component using function component and typescript

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,29 +1,35 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background: #c72828;
   padding: 30px 0;
 
+  background: #c72828;
+
   header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
     width: 1280px;
+
     margin: 0 auto;
     padding: 0 0 160px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
 
     nav {
       div {
         button {
-          font-weight: 600;
-          border-radius: 8px;
-          border: 0;
-          background: #39b100;
-          color: #fff;
-
           display: flex;
           flex-direction: row;
           align-items: center;
+
+          font-weight: 600;
+
+          border-radius: 8px;
+          border: 0;
+
+          color: #fff;
+
+          background: #39b100;
 
           .text {
             padding: 16px 24px;
@@ -31,10 +37,13 @@ export const Container = styled.div`
 
           .icon {
             display: flex;
+
             padding: 16px 16px;
-            background: #41c900;
-            border-radius: 0 8px 8px 0;
             margin: 0 auto;
+
+            border-radius: 0 8px 8px 0;
+
+            background: #41c900;
           }
         }
       }
